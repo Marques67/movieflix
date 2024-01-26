906a64a5e2f2a8d2a22c2b6a6216b96895e9ff95
lucasma2009_00 <lucas.marques@eurekalabs.com.br>
2024-01-26 16:57:57 -0300

    Ajustes no front

--- frontend/src/core/components/Routes/PrivateRoute.tsx
+++ frontend/src/core/components/Routes/PrivateRoute.tsx
@@ -1,37 +1,39 @@
 import { Route, Redirect } from 'react-router-dom';
-import { isAllowedByRole, isAuthenticated, Role } from '../../../core/utils/auth';
+import {
+  isAllowedByRole,
+  isAuthenticated,
+  Role,
+} from '../../../core/utils/auth';
 
 type Props = {
-  children: React.ReactNode
-  path: string
-  allowedRoutes?: Role[]
-  exact?: boolean
-}
+  children: React.ReactNode;
+  path: string;
+  allowedRoutes?: Role[];
+  exact?: boolean;
+};
 
 const PrivateRoute = ({ children, path, allowedRoutes, exact }: Props) => {
   return (
     <Route
-      path={ path }
+      path={path}
       render={({ location }) => {
         if (!isAuthenticated()) {
           return (
             <Redirect
               to={{
-                pathname: "/",
-                state: { from: location }
+                pathname: '/',
+                state: { from: location },
               }}
             />
-          )
+          );
         } else if (isAuthenticated() && !isAllowedByRole(allowedRoutes)) {
-          return (
-            <Redirect to={{ pathname: "/" }} /> // Redireciona o usuário para alguma rota caso ele não possa acessar a rota escolhida
-          )
+          return <Redirect to={{ pathname: '/' }} />;
         }
 
-        return children // Se falhar no if else libera a rota para todos os usuários
+        return children;
       }}
     />
   );
-}
+};
 
-export default PrivateRoute
+export default PrivateRoute;
--- frontend/src/pages/Home/CreateAccount/index.tsx
+++ frontend/src/pages/Home/CreateAccount/index.tsx
@@ -1,86 +1,92 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-import { makeRequest } from '../../../core/utils/requests'
-import { useHistory } from 'react-router'
-import AuthCard from '../components/AuthCard/AuthCard'
-import AuthCardButton from '../components/AuthCardButton'
+import { makeRequest } from '../../../core/utils/requests';
+import { useHistory } from 'react-router';
+import AuthCard from '../components/AuthCard/AuthCard';
+import AuthCardButton from '../components/AuthCardButton';
 
-import { ReactComponent as MainImage } from '../../../core/assets/main.svg'
-import './styles.scss'
-import { toast } from 'react-toastify'
+import { ReactComponent as MainImage } from '../../../core/assets/main.svg';
+import './styles.scss';
+import { toast } from 'react-toastify';
 
 type CreateAccountData = {
-  name: string
-  email: string
-  password: string
-}
+  name: string;
+  email: string;
+  password: string;
+};
 
 const CreateAccount = () => {
-  const { register, handleSubmit } = useForm<CreateAccountData>()
-  const [hasError, setHasError] = useState(false)
-  const history = useHistory()
+  const { register, handleSubmit } = useForm<CreateAccountData>();
+  const [hasError, setHasError] = useState(false);
+  const history = useHistory();
 
   const onSubmit = (createAccountData: CreateAccountData) => {
     const payload = {
       name: createAccountData.name,
       email: createAccountData.email,
       password: createAccountData.password,
-      roles: [{
-        id: 2 // Por padrão o usuário será membro para que a pessoa possa testar a aplicação
-      }]
-    }
+      roles: [
+        {
+          id: 2,
+        },
+      ],
+    };
 
     makeRequest({
       url: '/users',
       method: 'POST',
-      data: payload
-    }).then(() => {
-      toast.success('Conta criada com sucesso 🥳')
-      history.push('/login')
-    }).catch(() => {
-      toast.error('Erro ao tentar criar a conta 😢')
+      data: payload,
     })
-  }
+      .then(() => {
+        toast.success('Conta criada com sucesso 🥳');
+        history.push('/login');
+      })
+      .catch(() => {
+        toast.error('Erro ao tentar criar a conta 😢');
+      });
+  };
 
   return (
     <div className="home-container">
       <div className="home-content">
         <div className="home-left">
           <h1 className="home-title">Avalie Filmes</h1>
-          <h2 className="home-subtitle">Diga o que você achou do seu filme favorito</h2>
+          <h2 className="home-subtitle">
+            Diga o que você achou do seu filme favorito
+          </h2>
           <div className="home-image-container">
             <MainImage data-testid="main-image" />
           </div>
         </div>
 
         <AuthCard title="Criar Conta">
-          <form
-            onSubmit={ handleSubmit(onSubmit) }
-          >
+          <form onSubmit={handleSubmit(onSubmit)}>
             {hasError && (
               <div className="alert">
                 <p className="alert-text">Usuário ou senha inválidos!</p>
-                <span className="close" onClick={ () => setHasError(false) }>X</span>
+                <span className="close" onClick={() => setHasError(false)}>
+                  X
+                </span>
               </div>
             )}
 
             <input
-              {...register("name", { required: true },)}
+              {...register('name', { required: true })}
               type="text"
               placeholder="Nome"
               className="auth-input"
               data-testid="name"
             />
             <input
-              {...register("email", { required: true },)}
+              {...register('email', { required: true })}
               type="email"
               placeholder="Email"
               className="auth-input"
               data-testid="email"
             />
             <input
-              {...register("password", { required: true })}
+              {...register('password', { required: true })}
               type="password"
               placeholder="Senha"
               className="auth-input"
@@ -91,7 +97,7 @@ const CreateAccount = () => {
         </AuthCard>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateAccount
+export default CreateAccount;
